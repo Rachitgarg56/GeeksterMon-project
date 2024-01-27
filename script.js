7a9a0e9e9074ca456bce0ab86cb2a69dd2902618
Rachit Garg <rachitgarg56@gmail.com>
2024-01-27 13:24:15 +0530

    updated code

--- script.js
+++ script.js
@@ -1,84 +1,143 @@
 //model
 var pokemons = [];
 
+// window.onload = () => {
+  const select = document.querySelector("#select");
 
-window.onload = () => {
-
-    const select = document.querySelector('#select');
-
-    fetch('https://pokeapi.co/api/v2/type/')
-
+  fetch("https://pokeapi.co/api/v2/type/")
     .then((response) => response.json())
-    
+
     .then((parsedResponse) => {
+      const pokeTypesArr = parsedResponse.results;
 
-        const pokeTypesArr = parsedResponse.results;
-        
-        // fill the select with options 
-        for (let i = 0; i < pokeTypesArr.length; i++) {
-          const pokemonTypeObj = pokeTypesArr[i];
-          const pokemontype = pokemonTypeObj.name;
+      // fill the select with options
+      for (let i = 0; i < pokeTypesArr.length; i++) {
+        const pokemonTypeObj = pokeTypesArr[i];
+        const pokemontype = pokemonTypeObj.name;
 
-          const option = document.createElement('option');
-          option.setAttribute('value', pokemontype);
-          option.innerText = pokemontype;
-          select.append(option);
-        }
+        const option = document.createElement("option");
+        option.setAttribute("value", pokemontype);
+        option.innerText = pokemontype;
+        select.append(option);
+      }
 
-        return pokeTypesArr;
-    })
-
+      fetchPokeType(pokeTypesArr);
+    });
+// };
 
 
-    .then((pokeTypesArr) => {
+// for fetching types of pokemons
+function fetchPokeType(pokeTypesArr) {
+  pokeTypesArr.forEach((pokeTypeObj) => {
+    var pokeType = pokeTypeObj.name;
 
-        // traverse on types
-        pokeTypesArr.forEach(pokeTypeObj => {
+    // fetch url of each type
+    fetch(pokeTypeObj.url)
+      .then((response) => response.json())
 
-            var pokeType = pokeTypeObj.name;
+      .then((parsedResponse) => {
+        const pokemonsArr = parsedResponse.pokemon;
+        fetchPokemons(pokemonsArr, pokeType);
+      });
+  });
+}
 
-            // fetch url of each type
-            fetch(pokeTypeObj.url)
+// for fetching pokemons from each type
+function fetchPokemons(pokemonsArr, pokeType) {
+  // traverse on 10 pokemons
+  pokemonsArr.forEach(async (pokemon, idx) => {
+    const url = pokemon.pokemon.url;
+    if (idx < 10) {
+      //  fetch for 10 pokemons of each type
+      
+      fetch(url)
+        .then((res) => res.json())
 
-            .then((response) => response.json())
-
-            .then((parsedResponse) => {
-                const pokemonsArr = parsedResponse.pokemon;
-
-                // traverse on 10 pokemons 
-                for (var i=0; i<10; i++) {
-                    const url = pokemonsArr[i].pokemon.url;
-
-                    // fetch for 10 pokemons of each type
-                    fetch(url)
-                    .then((res) => res.json())
-
-                    //adding each pokemon data to model
-                    .then((parsedRes) => {
-                        pokemons.push({
-                            id: parsedRes.id,
-                            image: parsedRes.sprites.front_default,
-                            name: parsedRes.name,
-                            type: pokeType,
-                            abilities: parsedRes.abilities,
-                            // color: 
-                        })
-                        // return ((pokemons))
-                    })
-                    // .then((pokemons) => console.log(pokemons))
-
-                }
-
-                console.log(pokemons);
-            })
-
+        //adding each pokemon data to model
+        .then((parsedRes) => {
+          pokemons.push({
+            id: parsedRes.id,
+            image: parsedRes.sprites.front_default,
+            name: parsedRes.name,
+            type: pokeType,
+            abilities: parsedRes.abilities,
+            // color:
+          });
         });
+    }
+  });
 
-        // return pokemons;
-    })
+  console.log(pokemons);
+}
 
-    .then((pokemons) => {
-        console.log(pokemons);
-    })
+/////lokesh sirrrr
 
-};
+// const url = pokemon.pokemon.url;
+
+//           const geekmon = await fetch(url);
+
+//           const pokeData = await geekmon.json();
+
+//           pokemons.push({
+//                     id: pokeData.id,
+//                     image: pokeData.sprites.front_default,
+//                     name: pokeData.name,
+//                     type: pokeType,
+//                     abilities: pokeData.abilities,
+//                     // color:
+//           })
+
+//     .then((pokeTypesArr) => {
+//       // traverse on types
+//       pokeTypesArr.forEach((pokeTypeObj) => {
+//         var pokeType = pokeTypeObj.name;
+
+//         // fetch url of each type
+//         fetch(pokeTypeObj.url)
+//           .then((response) => response.json())
+
+//           .then((parsedResponse) => {
+//             const pokemonsArr = parsedResponse.pokemon;
+
+//             // traverse on 10 pokemons
+//             pokemonsArr.forEach((pokemon, idx) => {
+//               if (idx < 10) {
+//                 const url = pokemon.pokemon.url;
+
+//                 // fetch for 10 pokemons of each type
+//                 fetch(url)
+//                   .then((res) => res.json())
+
+//                   //adding each pokemon data to model
+//                   .then((parsedRes) => {
+//                     pokemons.push({
+//                       id: parsedRes.id,
+//                       image: parsedRes.sprites.front_default,
+//                       name: parsedRes.name,
+//                       type: pokeType,
+//                       abilities: parsedRes.abilities,
+//                       // color:
+//                     })
+//                     showPokemons(pokemons);
+
+//                   })
+
+//               }
+//             });
+
+//           });
+
+//         // .then ((pokemons) => console.log(pokemons))
+//       });
+
+//       // return pokemons;
+//     });
+//   // console.log(pokemons);
+//   // .then((pokemons) => {
+//   //     console.log(pokemons);
+//   // })
+// };
+
+// // function showPokemons(pokemonsArr) {
+// //   console.log(pokemonsArr);
+// // }
